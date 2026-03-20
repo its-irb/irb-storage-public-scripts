2a02bf0f887d1f3fad804ca2e071773aaa90a036
Ona Perez Prades <operez@irbbarcelona.pcb.ub.es>
2026-03-20 15:36:10 +0100

    test2 quitar onefile en windows

--- minio/bifrost.py
+++ minio/bifrost.py
@@ -1669,6 +1669,7 @@ def show_local_fs_modal(
     # Arrancar carga inicial después de que el modal esté en el árbol
     threading.Timer(0.1, refresh_fn).start()
 
+
 # ============================================================================
 # VISTA: INTERFAZ PRINCIPAL DE COPIA
 # ============================================================================
